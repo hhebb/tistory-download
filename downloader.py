d20615b612886622b7b21e74de3c65c9f6c773d4
CocoBerry12 <igotaspot426@gmail.com>
2021-01-16 19:17:36 +0900

    update

--- downloader.py
+++ downloader.py
@@ -45,20 +45,51 @@ def get_categories(url='/apis/category/list', token=None):
     return category_list
 
 
-def read_post(url='/apis/post/read', token=None):
-    url = 'https://www.tistory.com' + url
+def download_post(token, cnt):
+    url = 'https://www.tistory.com/apis/post/read'
     params = {
         'access_token': token,
         'output':'json',
         'blogName':'weflug',
-        'postId':215
+        'postId':cnt
     }
 
     res = requests.get(url, params)
     res = res.json()
-    document = res['tistory']['item']
-    document['categoryId'], document['content'], document['title']
+    try:
+        document = res['tistory']['item']
+        document['categoryId'], document['content'], document['title']
 
+        with open(f'post_{cnt}.html', 'w') as f:
+            f.write(document['content'])
+            pass
+        return document['title']
+    except:
+        return -1
+    
+
+def get_total_count(token):
+    url = 'https://www.tistory.com/apis/blog/info'
+    params = {
+        'access_token':token,
+        'output':'json'
+    }
+
+    res = requests.get(url, params)
+    blog_info = res.json()
+    total_post = blog_info['tistory']['item']['blogs'][1]['statistics']['post']
+    return total_post
+    
+def download_all_posts(token):
+    cnt = 1
+    inc = 1
+    total_post = get_total_count(token)
+    while cnt <= int(total_post):
+        post_entry = download_post(token, inc)
+        if post_entry != -1:
+            #print(post_entry)
+            cnt += 1
+        inc += 1
     
 if __name__ == "__main__":
     # 정보 로드
@@ -77,7 +108,10 @@ if __name__ == "__main__":
     print('wanna download?: ', flush=True)
     ans = sys.stdin.readline().strip()
     
-    if ans == 'y':
+    if ans == 'y' or ans == 'yes':
         print('downloading...')
+        download_all_posts(token)
     else:
         pass
+    
+    print(f'completed')
